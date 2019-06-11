e7f36a642874c3a411c43caa152173ef92b5af71
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-06-11 14:14:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let utils = require('./utils');
 
 module.exports = async function (context, req){
     let body = "";
-
+    
     // If client is requesting a webpage with article
     if(req.query.article) 
         body = ejs.render( 
@@ -39,10 +39,12 @@ async function dbSelectArticleById(id){
         // Select a specific row from article table by id, then close connection
         let pool = await sql.connect(utils.connectionObj);
         let result = await pool.request()
-            .input('id', sql.Int, utils.escapeHTML(id))
+            .input('title', sql.VarChar(128), utils.escapeHTML(id))
+
+            // re-write query to use new articlTag table, and get rid of "metaTag"
             .query(
-                'SELECT id, title, author, metaTags, coverImage, body, time_stamp \
-                FROM dbo.article WHERE id = @id'
+                'SELECT title, author, metaTags, coverImage, body, time_stamp \
+                FROM dbo.article WHERE title = @title'
             );
         pool.close();
         sql.close();
@@ -52,29 +54,40 @@ async function dbSelectArticleById(id){
         article.body = JSON.parse(article.body);
         article.metaTags = JSON.parse(article.metaTags);
         article.coverImage = JSON.parse(article.coverImage);
-        return JSON.stringify(article);
+        return utils.escapeHTML(JSON.stringify(article));
     } catch(e){return false;}
 }
 
 // Insert new article into DB
+// Insert all associated articleTags into DB
 async function dbInsertArticle(article){
     try{
         // Connect to DB and set up prepared statement query
-        // Insert a row into article table, then close connection
+        // Insert row into article DB table for new article
         let pool = await sql.connect(utils.connectionObj);
         let result = await pool.request()
             .input('title', sql.VarChar(128), utils.escapeHTML(article.title))
             .input('author', sql.VarChar(64), utils.escapeHTML(article.author))
             .input('metaTags', sql.VarChar(512), utils.escapeHTML(JSON.stringify(article.metaTags)))
             .input('coverImage', sql.VarChar(1024), utils.escapeHTML(JSON.stringify(article.coverImage)))
-            .input('body', sql.VarChar(8000), utils.escapeHTML(JSON.stringify(article.body)))
-            .query(
-                'INSERT INTO dbo.article (title, author, metaTags, coverImage, body) \
-                VALUES(@title, @author, @metaTags, @coverImage, @body)'
-            );
+            .input('body', sql.VarChar(8000), utils.escapeHTML(JSON.stringify(article.body)));
+        let batchInput = 'INSERT INTO dbo.article (title, author, metaTags, coverImage, body) \
+            VALUES(@title, @author, @metaTags, @coverImage, @body) ';
+
+        // For each articleTag, insert row into articleTag table
+        // Only insert first 5 tags and only if they are less than 24 chars
+        for(a = 0; a < article.metaTags.length && a < 5; a++){
+            if(article.metaTags[a].length <= 24){
+                batchInput += ' INSERT INTO dbo.articleTag (articleTitle, tag) \
+                    VALUES(@articleTitle' + a + ', @tag' + a + '); ';
+                result = result.input('articleTitle' + a, sql.VarChar(128), utils.escapeHTML(article.title))
+                    .input('tag' + a, sql.VarChar(24), utils.escapeHTML(article.metaTags[a]));
+            }
+        }
+        result = await result.query(batchInput);
+
         pool.close();
         sql.close();
-
-        return result.rowsAffected == 1 ? true : false;
+        return result.rowsAffected > 0 ? true : false;;
     } catch(e){return false;}
 }
