3952b62697d16fddd395168d58908dc7b50d7f3f
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-06-12 14:33:53 -0700

    Update markup.js

--- markup.js
+++ markup.js
@@ -8,16 +8,16 @@ function markup(str){
 		['``', '<sup>', '</sup>'],
 		['##', '<mark>', '</mark>']
 	];
-	
+
 	// For each markdown element, convert their occurences in str into corresponding HTML tag
-	// Every other conversion should be opening/closing tag
 	elems.forEach(elem => {
-		
+		elemCount = (str.match(elem[0], 'g') || []).length;
+
 		// Append closing tag to str if count of markdown elements is uneven
-		if((str.match(elem[0], 'g') || []).length % 2 != 0) str += elem[2];
-		
-		a = 0;
-		while(str.match(elem[0]))
+		if(elemCount % 2 != 0) str += elem[2];
+
+		// For a given elem, every other conversion in str should be opening/closing tag
+		for(a = 0; a < elemCount; a++)
 			str = a++ % 2 == 0 ? str.replace(elem[0], elem[1]) : str.replace(elem[0], elem[2]);
 	});
 	return str;
