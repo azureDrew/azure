7265b9686151e265ce91f1d4c6b698d74d003abb
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-06-12 09:47:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ module.exports = async function (context, req){
     if(req.query.article) 
         body = ejs.render( 
             fs.readFileSync(__dirname + "/article.ejs", 'utf-8'),
-            {articleJson: await dbSelectArticleById(req.query.article.trim())}
+            {articleJson: await dbSelectArticle(req.query.article.trim())}
         );
 
     // If client is requesting an article
     else if(req.query.getArticle)
-        body = await dbSelectArticleById(req.query.getArticle.trim());
+        body = await dbSelectArticle(req.query.getArticle.trim());
 
     // If client is posting an article
     else if(req.query.postArticle)
@@ -33,26 +33,25 @@ module.exports = async function (context, req){
 };
 
 // Get an article from DB by a given id
-async function dbSelectArticleById(id){
+async function dbSelectArticle(title){
     try{
         // Connect to DB and set up prepared statement query
         // Select a specific row from article table by id, then close connection
         let pool = await sql.connect(utils.connectionObj);
         let result = await pool.request()
-            .input('title', sql.VarChar(128), utils.escapeHTML(id))
-
-            // re-write query to use new articlTag table, and get rid of "metaTag"
+            .input('title', sql.VarChar(128), utils.escapeHTML(title))
             .query(
-                'SELECT title, author, metaTags, coverImage, body, time_stamp \
-                FROM dbo.article WHERE title = @title'
+                'SELECT title, author, coverImage, body, time_stamp \
+                FROM dbo.article WHERE title = @title; \
+                SELECT tag FROM articleTag WHERE articleTitle = @title;'
             );
         pool.close();
         sql.close();
 
         // Clean up / format DB result for JSON output and return result
-        let article = result.recordset[0];
+        let article = result.recordsets[0][0];
         article.body = JSON.parse(article.body);
-        article.metaTags = JSON.parse(article.metaTags);
+        article.metaTags = result.recordsets[1];
         article.coverImage = JSON.parse(article.coverImage);
         return utils.escapeHTML(JSON.stringify(article));
     } catch(e){return false;}
@@ -68,11 +67,10 @@ async function dbInsertArticle(article){
         let result = await pool.request()
             .input('title', sql.VarChar(128), utils.escapeHTML(article.title))
             .input('author', sql.VarChar(64), utils.escapeHTML(article.author))
-            .input('metaTags', sql.VarChar(512), utils.escapeHTML(JSON.stringify(article.metaTags)))
             .input('coverImage', sql.VarChar(1024), utils.escapeHTML(JSON.stringify(article.coverImage)))
             .input('body', sql.VarChar(8000), utils.escapeHTML(JSON.stringify(article.body)));
-        let batchInput = 'INSERT INTO dbo.article (title, author, metaTags, coverImage, body) \
-            VALUES(@title, @author, @metaTags, @coverImage, @body) ';
+        let batchInput = 'INSERT INTO dbo.article (title, author, coverImage, body) \
+            VALUES(@title, @author, @coverImage, @body) ';
 
         // For each articleTag, insert row into articleTag table
         // Only insert first 5 tags and only if they are less than 24 chars
@@ -80,7 +78,8 @@ async function dbInsertArticle(article){
             if(article.metaTags[a].length <= 24){
                 batchInput += ' INSERT INTO dbo.articleTag (articleTitle, tag) \
                     VALUES(@articleTitle' + a + ', @tag' + a + '); ';
-                result = result.input('articleTitle' + a, sql.VarChar(128), utils.escapeHTML(article.title))
+                result = result
+                    .input('articleTitle' + a, sql.VarChar(128), utils.escapeHTML(article.title))
                     .input('tag' + a, sql.VarChar(24), utils.escapeHTML(article.metaTags[a]));
             }
         }
@@ -88,6 +87,6 @@ async function dbInsertArticle(article){
 
         pool.close();
         sql.close();
-        return result.rowsAffected > 0 ? true : false;;
+        return result.rowsAffected > 0 ? true : false;
     } catch(e){return false;}
 }
