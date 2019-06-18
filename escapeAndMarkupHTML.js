60e10865f335161905b3d2d72d3813689976e8fa
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-06-18 09:38:05 -0700

    Update escapeAndMarkupHTML.js

--- escapeAndMarkupHTML.js
+++ escapeAndMarkupHTML.js
@@ -1,37 +1,37 @@
 // If open html tags from markup exist by the end of str, close them
 function closeHTMLtags(str){
-    var elems = [
-        ['&lt;!', '!&gt;'],
-        ['&lt;~', '~&gt;'],
-        ['&lt;-', '-&gt;'],
-        ['&lt;_', '_&gt;'],
-        ['&lt;`', '`&gt;'],
-        ['&lt;#', '#&gt;']
-    ];
+	var elems = [
+		['&lt;!', '!&gt;'],
+		['&lt;~', '~&gt;'],
+		['&lt;-', '-&gt;'],
+		['&lt;_', '_&gt;'],
+		['&lt;`', '`&gt;'],
+		['&lt;#', '#&gt;']
+	];
 
-    elems.forEach(elem => {
-        diff = 0;
-        for(a = 0; a < str.length - elem[1].length; a++){
-            if(str.substring(a, a + elem[0].length) == elem[0]) diff++;
-            else if(str.substring(a, a + elem[1].length) == elem[1] && diff > 0) diff--;
-        }
-        for(a = 0; a < diff; a++) str += elem[1];
-    });
+	elems.forEach(elem => {
+		diff = 0;
+		for(a = 0; a < str.length - elem[1].length; a++){
+			if(str.substring(a, a + elem[0].length) == elem[0]) diff++;
+			else if(str.substring(a, a + elem[1].length) == elem[1] && diff > 0) diff--;
+		}
+		for(a = 0; a < diff; a++) str += elem[1];
+	});
 
-    return str;
+	return str;
 }
 
 // Escape HTML characters...
 function escapeHTML(str){
-    return str.replace(
-        /[<>'"]/g,
-        tag => ({
-            '<': '&lt;',
-            '>': '&gt;',
-            "'": '&#039;',
-            '"': '&quot;'
-        }[tag] || tag)
-    );
+	return str.replace(
+		/[<>'"]/g,
+		tag => ({
+			'<': '&lt;',
+			'>': '&gt;',
+			"'": '&#039;',
+			'"': '&quot;'
+		}[tag] || tag)
+	);
 }
 
 // Replace escaped html for markdown characters with HTML tags
