e0af1d68547161713ca3d87c7ca1910ac74cbc9b
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-06-18 10:30:04 -0700

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,10 @@
+module.exports = {
+	// DB connectivity stuffs
+    connectionObj: {
+		user: 'azure-user-name',
+		database: 'azure-database-name',
+		password: 'azure-database-password',
+		server: 'azure-server-name.database.windows.net',
+		options: {encrypt: true}
+	}
+}
