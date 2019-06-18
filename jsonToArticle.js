49e18ee6ed529fd8799c80bb6d8502cd6bdd871f
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-06-18 09:04:39 -0700

    Update jsonToArticle.js

--- jsonToArticle.js
+++ jsonToArticle.js
@@ -7,24 +7,24 @@ else{
 		'<div class = "articleSubsectionContainer" id = "articleTags"></div>' +
 		'<div class = "articleSubsectionContainer" id = "articleBody"></div>';
 
-	document.title = articleJson.title;
-	document.getElementById("articleTitle").innerHTML = "<h2>" + articleJson.title + "<h2>";
-	document.getElementById("articleCoverImage").innerHTML = '<img class = "articleImage" src="' + articleJson.coverImage.url + '"><div class = "articleImageDescription">' + articleJson.coverImage.description + '</div>';
+	document.title = escapeHTML(articleJson.title);
+	document.getElementById("articleTitle").innerHTML = "<h2>" + escapeHTML(articleJson.title) + "<h2>";
+	document.getElementById("articleCoverImage").innerHTML = '<img class = "articleImage" src="' + escapeHTML(articleJson.coverImage.url) + '"><div class = "articleImageDescription">' + markupHTML(articleJson.coverImage.description) + '</div>';
 
 	t = articleJson.time_stamp.split(/[- :]/);
 	timeStamp = t[2] + ' ' + t[1] + ' ' + t[0];
-	document.getElementById("articleAuthor").innerHTML = 'By ' + articleJson.author + ' - ' + timeStamp;
+	document.getElementById("articleAuthor").innerHTML = 'By ' + escapeHTML(articleJson.author) + ' - ' + escapeHTML(timeStamp);
 
 	articleTags = "";
 	articleJson.tags.forEach(tag => 
-		articleTags += '<div class = "articleTag">' + tag.tag + '</div>'
+		articleTags += '<div class = "articleTag">' + escapeHTML(tag.tag) + '</div>'
 	);
 	document.getElementById("articleTags").innerHTML = articleTags + "<br>";
 
 	body = "";
 	articleJson.body.forEach(section => body += typeof section == "string" ? 
-		'<p class = "articleParagraph">' + section + '</p>' :
-		'<br><img class = "articleImage" src="' + section.url + '"><div class = "articleImageDescription">' + section.description + '</div><br>'
+		'<p class = "articleParagraph">' + markupHTML(section) + '</p>' :
+		'<br><img class = "articleImage" src="' + escapeHTML(section.url) + '"><div class = "articleImageDescription">' + markupHTML(section.description) + '</div><br>'
 	);
 	document.getElementById("articleBody").innerHTML = body;
 }
