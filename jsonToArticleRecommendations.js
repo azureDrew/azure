f8bcd7cebc0fbeaf705706ff2848c1ac3dea45d2
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-07-01 12:32:03 -0700

    Update jsonToArticleRecommendations.js

--- jsonToArticleRecommendations.js
+++ jsonToArticleRecommendations.js
@@ -1,7 +1,3 @@
-// Use cookie value to tell server most recently viewed article upon new articl request
-// Update cookie value when new article is received by client such that article chain continues. 
-document.cookie = "previousArticleViewedId = " + article.previousArticleViewedId;
-
 // Build HTML for each article being recommended to client
 articleRecommendations.forEach(rec => {
     href = 'article?code=vX4RJPEMviAFcEE4u7QV1ualG0KakeZGISTdVpLFMtd93e0v52f7jw==&article=' + rec.title + '&' + document.cookie;
