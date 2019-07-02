b55e3585d08b0d397b180ee4281254efe259bf68
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-07-02 11:46:25 -0700

    Update jsonToArticleRecommendations.js

--- jsonToArticleRecommendations.js
+++ jsonToArticleRecommendations.js
@@ -1,6 +1,6 @@
 // Build HTML for each article being recommended to client
 articleRecommendations.forEach(rec => {
-    href = 'article?code=vX4RJPEMviAFcEE4u7QV1ualG0KakeZGISTdVpLFMtd93e0v52f7jw==&article=' + rec.title + '&' + document.cookie;
+    href = 'article?code=vX4RJPEMviAFcEE4u7QV1ualG0KakeZGISTdVpLFMtd93e0v52f7jw==&article=' + rec.title + '&previousPage=' + window.location.href;
     document.getElementById("articleRecsContainer").innerHTML += '<a href = "' + href + '"><div class = "articleRecomendation">' + 
         '<img class = "articleThumbImage" src="' + escapeHTML(rec.coverImage.url) + '"><div class = "articleRecTitle">' + rec.title + '</div>' + 
     '</div></a>';
