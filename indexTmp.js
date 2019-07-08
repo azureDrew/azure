aec39143ed64129c03123837a828eec49ac2041e
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-07-08 15:22:04 -0700

    Update indexTmp.js

--- indexTmp.js
+++ indexTmp.js
@@ -24,12 +24,7 @@ module.exports = async function(context, req){
 
     // If client is posting article
     else if(req.postArticle)
-        body = await dbInsertArticle(utils.testArticle);
-        //body = await dbInsertArticle(JSON.parse(req.postArticle));
-
-    // If client is requesting article recommendations based on input article
-    else if(req.getArticleRecommendations)
-        body = await updateArticleRecommendations(req.getArticleRecommendations);
+        body = await dbInsertArticle(JSON.parse(req.postArticle));
 
     // If client is lost
     else body = false; // Replace with redirect to error.html once made
@@ -42,22 +37,31 @@ module.exports = async function(context, req){
     context.done();
 };
 
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////// Article Transactions ///////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
 // Get article from DB by given title
 // input: string, string
 async function dbSelectArticle(title, previousPage){
+    let maxTitleLen = 128;
+    let maxPreviousPageLen = 256;
+
     try{
         // Connect to DB with pool (if DNE) and set up prepared statement query
         // Select row from article table by title and all tags associated with it
         // Insert row into articleView table to log traffic, then output id for row
         pool = pool || await sql.connect(utils.connectionObj);
         let result = await pool.request()
-            .input('title', sql.VarChar(128), title)
-            .input('previousPage', sql.VarChar(256), previousPage)
+            .input('title', sql.VarChar(maxTitleLen), title)
+            .input('previousPage', sql.VarChar(maxPreviousPageLen), previousPage)
             .query(
                 'SELECT title, author, coverImage, body, time_stamp \
-                    FROM dbo.article WHERE title = @title; \
+                    FROM article WHERE title = @title; \
                 SELECT tag FROM articleTag WHERE articleTitle = @title; \
-                INSERT into dbo.articleViewed (articleTitle, previousPage) \
+                INSERT into articleViewed (articleTitle, previousPage) \
                     VALUES(@title, @previousPage) \
                 SELECT SCOPE_IDENTITY();'
             );
@@ -69,116 +73,6 @@ async function dbSelectArticle(title, previousPage){
         article.coverImage = JSON.parse(article.coverImage);
         return JSON.stringify(article);
     } catch(e){return false;}
-}
-
-// Get recommended articles similar to input article
-// input: string
-async function dbSelectArticleRecommendations(title){
-    let maxAge = 1000000; // microseconds
-
-    try{
-        // Connect to DB with pool (if DNE) and set up prepared statement query
-        // Select most recent article recommendations for given article
-        pool = pool || await sql.connect(utils.connectionObj);
-        let result = await pool.request()
-            .input('title', sql.VarChar(128), title)
-            .query(
-                'SELECT TOP (1) recommendations, time_stamp FROM dbo.articleRecommendations \
-                WHERE articleTitle = @title ORDER BY id DESC;'
-            );
-        
-        // If article does not have recommendations associated with it 
-        // or its recommendations have not been updated recently, 
-        // update its recommendations, then return result
-        if(result.recordsets[0].length == 0){
-            updateArticleRecs(title);
-            return false;
-        } else {
-            let currentTime = (new Date).getTime();
-            let lastUpdate = (new Date(result.recordsets[0][0].time_stamp)).getTime();
-            if(currentTime - lastUpdate > maxAge) updateArticleRecommendations(title);
-            return result.recordsets[0][0].recommendations;
-        }
-    } catch(e){return false;}
-}
-
-// Update the recommendations associated with a given article
-async function updateArticleRecommendations(title){
-    /*let bloom1 = new bf.BloomFilter(32, 4);
-    let bloom2 = new bf.BloomFilter(32, 4);
-
-    bloom1.add('a');
-    bloom1.add('b');
-    bloom2.add('a');
-    bloom2.add('b');
-
-    bloom1.add('c');
-    bloom2.add('c');
-    bloom2.add('d');
-
-    tmp1 = [].slice.call(bloom1.buckets)[0] >>> 0;
-    tmp2 = [].slice.call(bloom2.buckets)[0] >>> 0;
-
-    output = ((tmp1 & tmp2) >>> 0).toString(2).match(/1/g).length;
-    return output;
-    */
-
-    /*
-        order = []
-
-        // order result such that the final "order" array's individual cells are ordered
-        // such that most viewed articles are last. Once "order" is flattened, most viewed
-        // articles will be towards the back of the array... increasing their chance of
-        // being picked
-        query select all articles from DB, order by viewcount ascending
-        
-        aFilter = query.pop(article with title = title)
-        foreach result in query: r
-            similarity = ((aFilter & r.filter) >>> 0).toString(2).match(/1/g).length
-            order[similrity].push({
-                title: r.title,
-                coverImage: r.coverImage
-            })
-        order = order.flattanArray()
-        return 3 randomly chosen articles from last 10 in "order"
-    */
-
-    order = [];
-
-    try{
-        // Connect to DB with pool (if DNE) and set up prepared statement query
-        // Select all articles and relavent data for recommendations
-        pool = pool || await sql.connect(utils.connectionObj);
-        let result = await pool.request()
-            .input('title', sql.VarChar(128), title)
-            .query(
-                'SELECT title, bloomFilter, coverImage FROM dbo.article; \
-                SELECT bloomFilter FROM dbo.article WHERE title = @title;'       
-            );
-        
-        // If input article exists
-        if(result.recordsets[1].length == 1){
-            aFilter = result.recordsets[1][0].bloomFilter;
-            if(aFilter == null){
-                aFilter = buildArticleFilter(tags);
-                // update article row with created filter
-            }
-
-            /*
-            // compare all article filters to given article
-            // sort comparisons and return best of
-            result.recordsets[0].forEach(row => 
-                if(row.bloomFilter){
-                    similarity = ((aFilter & r.filter) >>> 0).toString(2).match(/1/g).length;
-                    order[similrity].push({
-                        title: r.title,
-                        coverImage: r.coverImage
-                    });
-                }
-            );*/
-            return "tmp";
-        } else return false;
-    } catch(e){return e;}
 }
 
 // Insert new article into DB
@@ -207,14 +101,14 @@ async function dbInsertArticle(article){
             .input('author', sql.VarChar(maxAuthorLen), article.author)
             .input('coverImage', sql.VarChar(maxImageLen), JSON.stringify(article.coverImage))
             .input('body', sql.VarChar(maxBodyLen), JSON.stringify(article.body));
-        let batchInput = 'INSERT INTO dbo.article (title, author, coverImage, body) \
+        let batchInput = 'INSERT INTO article (title, author, coverImage, body) \
             VALUES(@title, @author, @coverImage, @body) ';
         
         // For each articleTag, insert row into articleTag table
         // Only insert first maxNumOfTags tags and only if article insert was successful
         batchInput += ' IF @@ROWCOUNT = 1 BEGIN ';
         for(a = 0; a < article.tags.length && a < maxNumOfTags; a++){ 
-            batchInput += ' INSERT INTO dbo.articleTag (articleTitle, tag) \
+            batchInput += ' INSERT INTO articleTag (articleTitle, tag) \
                 VALUES(@articleTitle' + a + ', @tag' + a + '); ';
             result = result
                 .input('articleTitle' + a, sql.VarChar(maxTitleLen), article.title)
@@ -227,6 +121,12 @@ async function dbInsertArticle(article){
     } catch(e){return false;}
 }
 
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////// Recommendations Functionality /////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
 // Create (4 hash, 32 bit) bloom filter for article
 // Use article tags to determin set membership
 // input: [string, ...]
@@ -236,5 +136,106 @@ async function buildArticleFilter(tags){
     let bloom = new bf.BloomFilter(32, 4);
     for(a = 0; a < tags.length && a < maxNumOfTags; a++)
         bloom.add(tags[a]);
-    return [].slice.call(bloom.buckets)[0];
+    return ([].slice.call(bloom.buckets)[0] >>> 0);
+}
+
+// Get recommended articles from articleRecommendations table
+// Update recommendations if necessary
+// input: string
+async function dbSelectArticleRecommendations(title){
+    let maxAge = 1000000; // microseconds
+    let maxTitleLen = 128;
+
+    try{
+        // Connect to DB with pool (if DNE) and set up prepared statement query
+        // Select most recent article recommendations for given article
+        pool = pool || await sql.connect(utils.connectionObj);
+        let result = await pool.request()
+            .input('title', sql.VarChar(maxTitleLen), title)
+            .query(
+                'SELECT TOP (1) recommendations, time_stamp FROM articleRecommendations \
+                WHERE articleTitle = @title ORDER BY id DESC;'
+            );
+        
+        // If article does not have recommendations associated with it 
+        // or its recommendations have not been updated recently, 
+        // update its recommendations, but return existing result before update completes
+        if(result.recordsets[0].length == 0){
+            updateArticleRecommendations(title);
+            return false;
+        } else {
+            let currentTime = (new Date).getTime();
+            let lastUpdate = (new Date(result.recordsets[0][0].time_stamp)).getTime();
+            if(currentTime - lastUpdate > maxAge) updateArticleRecommendations(title);
+            return result.recordsets[0][0].recommendations;
+        }
+    } catch(e){return false;}
+}
+
+// Update recommendations associated with given article
+async function updateArticleRecommendations(title, numRecommendations = 3){
+    let maxTitleLen = 128;
+    let maxRecommendationsLen = 1024;
+
+    try{
+        // Connect to DB with pool (if DNE) and set up prepared statement query
+        // Select all articles and relavent data for recommendations
+        pool = pool || await sql.connect(utils.connectionObj);
+        let result = await pool.request()
+            .input('title', sql.VarChar(maxTitleLen), title)
+            .query(
+                'SELECT title, bloomFilter, coverImage FROM article; \
+                SELECT bloomFilter FROM article WHERE title = @title;'       
+            );
+        
+        // If input article does not exists, return false
+        // Otherwise, store results and continue
+        if(result.recordsets[1].length != 1) return false;
+        let aFilter = result.recordsets[1][0].bloomFilter;
+        let allFilters = result.recordsets[0];
+
+        // If filter DNE for an article that does exist, create its filter
+        if(typeof aFilter != 'number'){
+            // Get tags of article, then create filter based on tags
+            let tags = [];
+            result = await pool.request()
+                .input('title', sql.VarChar(maxTitleLen), title)
+                .query('SELECT tag FROM articleTag WHERE articleTitle = @title;');
+            result.recordsets[0].forEach(obj => tags.push(obj.tag));
+            aFilter = await buildArticleFilter(tags);
+
+            // Update article row with created filter
+            result = await pool.request()
+                .input('title', sql.VarChar(maxTitleLen), title)
+                .input('bloomFilter', sql.Int, aFilter)
+                .query('UPDATE article SET bloomFilter = @bloomFilter WHERE title = @title;');
+        }
+
+        // Compare all article filters to given article filter using bloom filter hamming distances
+        // Sort comparisons such that best recommendations appear last in array
+        let order = new Array(32).fill(null);
+        while(row = allFilters.pop()){
+            if(typeof row.bloomFilter == 'number'){
+                similarity = (aFilter & row.bloomFilter).toString(2).match(/1/g).length;
+                if(order[similarity] == null) order[similarity] = [];
+                order[similarity].push({
+                    title: row.title,
+                    coverImage: JSON.parse(row.coverImage)
+                });
+            }
+        }
+
+        // Flatten order array and store last "numRecommendations" elements
+        // Store new recommendations in articleRecommendations table and return query result
+        let recommendations = [].concat(...order.filter(e => e != null)).slice(-numRecommendations);
+        recommendations = JSON.stringify(recommendations);
+        result = await pool.request()
+            .input('title', sql.VarChar(maxTitleLen), title)
+            .input('recommendations', sql.VarChar(maxRecommendationsLen), recommendations)
+            .query(
+                'INSERT INTO articleRecommendations (articleTitle, recommendations) \
+                VALUES(@title, @recommendations)'
+            );
+        return result.rowsAffected[0] == 1 ? true : false;
+    } catch(e){return false;}
 }
