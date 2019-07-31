fa218e488e8fe38749877375dd8c1c96ebe28760
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-07-31 15:56:52 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,37 @@
 f(userId, type, superType, superObjectId, obj){
-	if(
-		typeof userId != int ||
-		typeof type != string ||
-		typeof superType != string ||
-		typeof superObjectId != int
-	) return false;
-
-	pool = pool || await sql.connect(utils.connectionObj);
-	let table = superType + upper(type);
-	let result = await pool.request()
-		.input("userId", sql.int, userId)
-		.input("superId", sql.int, superObjectId)
-	if(type == "comment")
-		result = await result
-			.input("parrentCommentId", sql.int, obj.parrentCommentId)
-			.input("body", sql.varchar(size), obj.body)
-			.query(
-				"insert into " + table + " 
-				(userId, superId, parrentCommentId, body) 
-				VALUES(@userId, @superId, @parentCommentId, @body)"
-			);
-	else if(type == "tag")
-		result = await result
-			.input("body", sql.varchar(size), obj.body)
-			.query(
-				"insert into " + table + " 
-				(userId, superId, body) 
-				VALUES(@userId, @superId, @body)"
-			);
-	else return false;
-	return result.rowsAffected[0] == 1 ? true : false;
+	try{
+		pool = pool || await sql.connect(utils.connectionObj);
+		
+		// check for existance of superObject first
+		// If DNE, return false, otherwise do below
+		
+		// Look up if foriegn key constraint will enforce above automatically
+		
+		let result = await pool.request()
+			.input("table", sql.VarChar(), camalCase([superType, type]))
+			.input("userId", sql.Int, userId)
+			.input("superId", sql.Int, superObjectId)
+		if(type == "comment")
+			result = await result
+				.input("parrentCommentId", sql.Int, obj.parrentCommentId)
+				.input("body", sql.VarChar(MAX_COMMENT_LEN), obj.body)
+				.query(
+					`insert into @table 
+					(userId, superId, parrentCommentId, body)  
+					VALUES(@userId, @superId, @parentCommentId, @body)`
+				);
+		else if(type == "tag")
+			result = await result
+				.input("body", sql.VarChar(MAX_TAG_LEN), obj.body)
+				.query(
+					`insert into @table 
+					(userId, superId, body) 
+					VALUES(@userId, @superId, @body)`
+				);
+		else return false;
+		return result.rowsAffected[0] == 1 ? true : false;
+	}catch(e){
+		// log error then return false
+		return false;
+	}
 }
