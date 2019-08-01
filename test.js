1b1fd1cb63efb12e8245593b0229b1c55511176f
azureDrew <51385995+azureDrew@users.noreply.github.com>
2019-08-01 13:35:41 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ let dbFieldTypeMap = {
     groupMemberId: sql.Int,
     commentId: sql.Int,
     tagId: sql.Int,
-    title: sql.VarChar(256)
+    title: sql.VarChar(256), 
     description: sql.VarChar(2048),
     imageUrl: sql.VarChar(256),
     status: sql.TinyInt
@@ -14,29 +14,25 @@ let dbFieldTypeMap = {
 }
 
 // Insert "entries" into "table" in DB.
-async function dbInsert(data){
+async function dbInsert(table, entries){
     try{
-        let table = data[0];
-        let entries = data[1];
-
         // Set up connection pool and establish query request
         pool = pool || await sql.connect(utils.connectionObj);
-        let result = await pool.request();
+        let result = await pool.request().input("table", "dbo." + table);
 
         // Create strings for the sql query.
         // Build out prepared statement (result).
         let columns = " (";
         let values = " VALUES( ";
-        entries.forEach(entry => {
-            columns += entry.field + ", ";
-            values += "@" + entry.field + ", ";
+        let entriesLength = entries.length() - 1;
+        entries.forEach((entry, counter) => {
+            columns += entry.field + (counter != entriesLength ? ", " : ") ");
+            values += "@" + entry.field + (counter != entriesLength ? ", " : ") ");
             result = result.input(entry.field, dbFieldTypeMap[entry.field], entry.val);
         });
-        values = values.slice(0, -2) + ") ";
-        columns = columns.slice(0, -2) + ") ";
 
         // Attempt insert and return success or failure result.
-        result = await result.query('INSERT INTO dbo.' + table + columns + values);
+        result = await result.query('INSERT INTO @table ' + columns + values);
         return result.rowsAffected == 1 ? true : false;
     } catch(e) {
         // log error and return false
